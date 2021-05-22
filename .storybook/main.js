cb27fe2949f6a492eeda8d54b733282edfda777a
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2021-05-22 19:18:04 +0700

    add sb

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    "../src/web/_sb_web/**/*.stories.mdx",
+    "../src/web/_sb_web/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { registerRootComponent } from "expo";
 
 import App from "./src/native/App.native";
+import StorybookUIRootfrom from "./src/native/_sb_native";
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
-// the environment is set up appropriately
+// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// // It also ensures that whether you load the app in the Expo client or in a native build,
+// // the environment is set up appropriately
+
+// For SB
+// registerRootComponent(StorybookUIRootfrom);
 registerRootComponent(App);
